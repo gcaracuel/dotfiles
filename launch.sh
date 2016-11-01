d31aa2fb7012d9b7ff31538ea537aaa6d4444b79
Guillermo Caracuel <gcaracuel@gmail.com>
2016-11-01 23:17:52 +0100

    Ansible bootstraping first version

---
+++ launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git pull
+# Requirement:   sudo pip install --upgrade pip install git+git://github.com/ansible/ansible.git@stable-2.2
+ansible-playbook -i ansible/inventory ansible/bootstrap.yml
+
+#if [[ `uname -s` == 'Linux' ]]; then
+#  if [[ `gawk -F= '/^NAME/{print $2}' /etc/os-release` == 'fedora' ]]; then
+#    ansible-playbook -i ansible/inventory ansible/fedora.yml
+#  elif [[ `gawk -F= '/^NAME/{print $2}' /etc/os-release` == 'ubuntu' ]]; then
+#    ansible-playbook -i ansible/inventory ansible/ubuntu.yml
+#  else
+#    echo "Not support Linux distribution"
+#  fi
+#else
+#  ansible-playbook -i ansible/inventory ansible/osx.yml
+#fi
