1b0021465260e3f995e714f1ce7d08b86f111c0b
Guillermo Caracuel <gcaracuel@riplife.es>
2016-11-04 17:00:37 +0100

    Verified Fedora

--- launch.sh
+++ launch.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
-
-# Requirements:
-# git & ansible
-
+echo "
+# Requirements:\n
+#   (Fedora): sudo dnf install ansible git python2-dnf\n
+#   (OSX): sudo pacman -S ansible\n"
 
 git pull
-ansible-playbook -i ansible/inventory ansible/bootstrap.yml
-
-#if [[ `uname -s` == 'Linux' ]]; then
-#  if [[ `gawk -F= '/^NAME/{print $2}' /etc/os-release` == 'fedora' ]]; then
-#    ansible-playbook -i ansible/inventory ansible/fedora.yml
-#  elif [[ `gawk -F= '/^NAME/{print $2}' /etc/os-release` == 'ubuntu' ]]; then
-#    ansible-playbook -i ansible/inventory ansible/ubuntu.yml
-#  else
-#    echo "Not support Linux distribution"
-#  fi
-#else
-#  ansible-playbook -i ansible/inventory ansible/osx.yml
-#fi
+ansible-playbook -i ansible/inventory ansible/bootstrap.yml $1
