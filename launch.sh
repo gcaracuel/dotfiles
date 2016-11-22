7651cfcc93db80e8a3a8f56e3a0f625ca81a2686
Guillermo Caracuel <gcaracuel@gmail.com>
2016-11-22 10:31:04 +0100

    Use my personal Bureau theme version

--- launch.sh
+++ launch.sh
@@ -5,11 +5,10 @@
 #  ./launch.sh -v  OR   cd ansible && ansible-playbook -i inventory bootstrap.yml
 #  (Only update dotfiles)  ./launch.sh  "-v --tags dotfieles"  OR  cd ansible && ansible-playbook -i inventory bootstrap.yml --tags dotfiles
 
-
 echo "
 # Requirements:\n
 #   (Fedora): sudo dnf install ansible git\n
 #   (OSX): sudo easy_install pip && sudo pip install --upgrade pip && sudo pip install --upgrade ansible\n"
 
 git pull
-cd ansible && ansible-playbook -i inventory bootstrap.yml $1
+cd ansible && ansible-playbook -i inventory bootstrap.yml -K -v $1
